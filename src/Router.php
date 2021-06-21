640c98d5077e46a125c9722f195d9462d6786375
Mihail Binev <kodeart@gmail.com>
2021-06-21 13:26:44 +0200

    - refactor the implementation logic and cleanup

--- src/Router.php
+++ src/Router.php
@@ -52,9 +52,7 @@ class Router
         }
         foreach ($this->index as $id => $route) {
             if (\preg_match($route['regex'], $path, $params)) {
-                return $this->normalizeParams(
-                    $this->callback[$id] ?? $route, $params
-                );
+                return $this->normalizeParams($this->callback[$id] ?? $route, $params);
             }
         }
         return [];
@@ -89,19 +87,7 @@ class Router
             'template' => $template,
             'resource' => \is_object($resource) ? '' : $resource,
         ];
-        [$route['regex'], $identity] = $this->compileTemplate($template);
-        isset($this->identity[$identity]) && throw (new HTTPConflict(
-            instance: $template,
-            title: 'Duplicate route',
-            detail: \preg_replace(
-                '/[' . PHP_EOL . ' ]+/', ' ', \sprintf(
-                    'Detected a multiple route definitions. The URI template for route "%s" 
-                    conflicts with already defined route "%s". Please fix your routes.',
-                    $template, $this->identity[$identity]))
-        ))->setMember('conflict-route', [$template => $this->identity[$identity]]);
-
-        $route['identity'] = $identity;
-        $this->identity[$identity] = $template;
+        [$route['regex'], $route['identity']] = $this->compileTemplate($template);
         $this->index[$id] = $route;
         if (empty($route['resource'])) {
             $this->callback[$id] = ['resource' => $resource] + $route;
@@ -115,14 +101,46 @@ class Router
         if (false ===\str_contains($template, '{') && false ===\str_contains($template, '<')) {
             return ['~^' . \preg_quote($template, '/') . '$~ui', $template];
         }
-        [$regex, $identity, $options] = $this->processMatches($template);
+        $options = '';
+        $regex = $identity = $template;
+        $types = [
+            'str' => '.+?', // non-greedy (stop at first match)
+            'int' => '\-?\d+',
+            'float' => '(\-?\d*\.\d+)',
+            'path' => '.+', // greedy
+            'regex' => '',
+            'uuid' => UUID::PATTERN,
+        ];
+
+        // https://regex101.com/r/xeuMU3/2
+        preg_match_all('~{((?:[^{}]*|(?R))*)}~mx',
+                       $template,
+                       $parameters,
+                       PREG_SET_ORDER);
+
+        foreach ($parameters as [$parameter, $param]) {
+            [$name, $type, $filter] = explode(':', $param, 3) + [1 => 'str', 2 => ''];
+            $this->assertSupportedType($types, $type, $filter, $template);
+            $expr = $filter ?: $types[$type];
+            $regex = \str_replace($parameter, '(?P<' . $name .'>' . $expr . ')', $regex);
+            $identity = \str_replace($parameter, $types[$type] ? ":$type" : $filter, $identity);
+            ('str' === $type || 'path' === $type) && $options = 'ui';
+        }
+        /*
+         * [NOTE]: Replace :path with :str. The concept of "path" is irrelevant
+         *  because the single parameters are matched as non-greedy (first occurrence)
+         *  and the path is greedy matched (as many as possible). The implementation
+         *  cannot distinguish between both types, therefore limit the types to :str
+         *  and disallow routes with /:str/:path and /:str/:str identities.
+         */
+        $identity = \str_replace(':path', ':str', $identity, $paths);
+        $this->assertIdentityAndPaths($identity, $template, $paths);
+
         try {
             $regex = '~^' . $regex . '$~' . $options;
-
             // TODO: Quick test for duplicate subpattern names
             \preg_match($regex, '/');
             return [$regex, $identity];
-
         } catch (\Throwable $ex) {
             throw new HTTPConflict(
                 title: 'PCRE compilation error. ' . $ex->getMessage(),
@@ -132,63 +150,36 @@ class Router
         }
     }
 
-    private function processMatches(string $template): array
+    private function assertSupportedType(array $types, string $type, string $filter, string $template): void
     {
-        $types = [
-            ':str' => '.+?', // non-greedy (stop at first match)
-            ':path' => '.+', // greedy
-            ':int' => '\-?\d+',
-            ':float' => '(\-?\d*\.\d+)',
-            ':uuid' => UUID::PATTERN,
-            ':regex' => '',
-        ];
-
-        // https://regex101.com/r/xeuMU3/2
-        preg_match_all('~{((?:[^{}]*|(?R))*)}~mx',
-                       $template,
-                       $parameters,
-                       PREG_SET_ORDER);
-
-        $options = '';
-        $regex = $identity = $template;
-        foreach ($parameters as [$parameter, $param]) {
-            [$name, $type, $filter] = explode(':', $param, 3) + [1 => 'str', 2 => ''];
-
-            ('regex' === $type && empty($filter)) && throw new HTTPConflict(
-                title: 'Invalid route. No regular expression provided',
-                detail: 'Provide a proper PCRE regular expression',
-                instance: $template,
-            );
-
-            isset($types[":$type"]) || throw (new HTTPConflict(
-                title: \sprintf('Invalid route parameter type %s', $type),
-                detail: 'Use one of the supported parameter types',
-                instance: $template,
-            ))->setMember('supported-types', \array_keys($types));
-
-            $expr = $filter ?: $types[":$type"];
-
-            $regex = \str_replace($parameter, "(?P<$name>$expr)", $regex);
-            $identity = \str_replace($parameter, $types[":$type"] ? ":$type" : ''.$filter, $identity);
-            if ('str' === $type || 'path' === $type) {
-                $options = 'ui';
-            }
-        }
-        /*
-         * [NOTE]: Replace :path with :str. The concept of "path" is irrelevant
-         *  because the single parameters are matched as non-greedy (first occurrence)
-         *  and the path is greedy matched (as many as possible). The implementation
-         *  cannot distinguish between both types, therefore limit the types to :str
-         *  and disallow routes with /:str/:path and /:str/:str identities.
-         *
-         *  Also check for multiple :path parameters in the URI template.
-         */
-        $identity = \str_replace(':path', ':str', $identity, $count);
-        if ($count > 1) throw new HTTPConflict(
-            title: 'Invalid route. Multiple path parameters in the route template detected',
-            detail: 'Only one ":path" type is allowed as URI parameter',
+        ('regex' === $type && empty($filter)) && throw new HTTPConflict(
+            title: 'Invalid route. No regular expression provided',
+            detail: 'Provide a proper PCRE regular expression',
             instance: $template,
         );
-        return [$regex, $identity, $options];
+        isset($types[$type]) || throw (new HTTPConflict(
+            title: \sprintf('Invalid route parameter type %s', $type),
+            detail: 'Use one of the supported parameter types',
+            instance: $template,
+        ))->setMember('supported-types', \array_keys($types));
+    }
+
+    private function assertIdentityAndPaths(mixed $identity, string $template, int $paths): void
+    {
+        isset($this->identity[$identity]) && throw (new HTTPConflict(
+            instance: $template,
+            title: 'Duplicate route',
+            detail: \preg_replace('/[' . PHP_EOL . ' ]+/', ' ', \sprintf(
+                        'Detected a multiple route definitions. The URI template for route "%s" 
+                conflicts with already defined route "%s". Please fix your routes.',
+                        $template, $this->identity[$identity]))
+        ))->setMember('conflict-route', [$template => $this->identity[$identity]]);
+
+        if ($paths > 1) throw new HTTPConflict(
+            title: 'Invalid route. Multiple path parameters in the route template detected',
+            detail: 'Only one "path" type is allowed as URI parameter',
+            instance: $template,
+        );
+        $this->identity[$identity] = $template;
     }
 }
